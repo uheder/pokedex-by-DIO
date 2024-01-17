faf36b9a5bef378b4139420bca898c74edf78747
Uheder Macedo <dev.usmacedo@outlook.com>
2024-01-17 18:16:50 -0300

    fix: corrigido codigo para carregar imagens da API

--- app.py
+++ app.py
@@ -5,26 +5,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    api_url = 'https://pokeapi.co/api/v2/pokemon?limit=365/'
+    api_url = 'https://pokeapi.co/api/v2/pokemon?limit=10'
     response = requests.get(api_url)
 
     if response.status_code == 200:
         data = response.json()
         pokemons = data.get('results', [])
+
+        for pokemon in pokemons:
+            details = get_poke_details(pokemon['url'])
+            if details:
+                pokemon.update(details)
+
+
         return render_template('index.html', pokemons = pokemons)
     return 'Erro na solicitação à API'
 
 def get_poke_details(pokemon_url):
-    response = requests.get(pokemon_url)
+    detail_response = requests.get(pokemon_url)
 
-    if response.status_code == 200:
-        data = response.json()
-        # image_url = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/{data.get("id","")}.svg'
+    if detail_response.status_code == 200:
+        detail_data = detail_response.json()
 
         return {
-            'type': data.get('types', []),
-            'moveset': data.get('moves', []),
-            'id': data.get('id', []),
-            # 'image_url': image_url,
+            'type': detail_data.get('types', []),
+            'moveset': detail_data.get('moves', []),
+            'id': detail_data.get('id', 0),
         }
     return None
